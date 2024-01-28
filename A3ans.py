81585926cf621f3fb0b43723181dfd7c2e3e355e
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-01-29 01:59:00 +0530

    Update A3ans.py

--- A3ans.py
+++ A3ans.py
@@ -7,3 +7,11 @@ ans
 
 model_names = ans['Model Names']
 topsis_scores = ans['TOPSIS_Score']
+plt.figure(figsize=(10, 6))
+colors = sns.color_palette('viridis', len(df[category_column]))
+plt.bar(model_names, topsis_scores, color=colors)
+plt.xlabel('Model Name')
+plt.ylabel('Topsis Score')
+plt.title('TOPSIS Evaluation for various text classification models - By Akul Vaishnavi (102103487)')
+plt.tight_layout()
+plt.show()
